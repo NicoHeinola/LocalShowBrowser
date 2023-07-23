10528687af053eeb2d1f62c4f4c97583822180fb
Nico Heinola <nico.heinola@gmail.com>
2023-07-23 11:46:55 +0300

    Fix

--- controllers/show/show_controller.py
+++ controllers/show/show_controller.py
@@ -44,7 +44,7 @@ class ShowController(BaseController):
                 shows = []
             else:
                 # results = db.session.query(User, Post).join(Post).filter(User.id == Post.user_id).all()
-                shows = db.session.query(Show).join(UserOpenedShow) Show.query.order_by(Show.created_at.desc()).limit(3).all()
+                shows = Show.query.order_by(Show.created_at.desc()).limit(3).all()
             return make_response(jsonify([show.serialize for show in shows]), 200)
 
         @self._app.route(base_name + '/<id>', methods=['GET'])
